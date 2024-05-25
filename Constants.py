87bd1db2fb0dace260c8b7517e30bdb19a2b776c
ElenavBG <113910725+ElenavBG@users.noreply.github.com>
2024-05-25 18:26:17 +0200

    Add files via upload

--- Constants.py
+++ Constants.py
@@ -4,17 +4,17 @@ Created on Sat May 25 17:57:57 2024
 
 @author: Elena
 """
-price_gasoline = 0,1791666667
-price_ebike = 0,0016
-price_Ecar = 0,02
+price_gasoline = 0.,1791666667
+price_ebike = 0.0016
+price_Ecar = 0.02
 d_max_ebike = 50
-wage = 13,27
+wage = 13.27
 t_working = 
 emissions_car = 95
-t_max_bike = 0,75
+t_max_bike = 0.75
 t_max_car = 2
-t_max_walking = 0,75
-t_stop = 0,1666666667
+t_max_walking = 0.75
+t_stop = 0.1666666667
 vs_cars = 50
 vs_normal = 15
 vs_ebike = 25
